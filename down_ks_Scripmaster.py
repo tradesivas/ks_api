4cfdd8ca576bd9c27f533f6e5afb16429ac01bb9
tradesivas <tradesivas@gmail.com>
2023-02-08 10:15:20 +0530

    update

--- down_ks_Scripmaster.py
+++ down_ks_Scripmaster.py
@@ -1,11 +1,13 @@
 import requests
 import os
-from dotenv import load_dotenv
-load_dotenv()
-fno_url = os.getenv("fno_url")
-cash_url = os.getenv("cash_url")
-fno_file = os.getenv("fno_file")
-cash_file = os.getenv("cash_file")
+import datetime
+from datetime import date
+todays_date = date.today()
+tdate = str(todays_date.day).zfill(2)+'_'+str(todays_date.month).zfill(2)+'_'+str(todays_date.year)
+fno_url = 'https://preferred.kotaksecurities.com/security/production/TradeApiInstruments_FNO_'+tdate+'.txt'    #change needed everyday
+cash_url = 'https://preferred.kotaksecurities.com/security/production/TradeApiInstruments_CASH_'+tdate+'.txt'
+fno_file = 'ks_fno_scripmaster.txt'
+cash_file = 'ks_cash_scripmaster.txt'
 fno = requests.get(fno_url, allow_redirects=True)
 open(fno_file, 'wb').write(fno.content)
 cash = requests.get(cash_url, allow_redirects=True)
