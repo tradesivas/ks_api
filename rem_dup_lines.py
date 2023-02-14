fe1c43a974c9fa3d4bd34c60a38c41ab9f034392
tradesivas <tradesivas@gmail.com>
2023-02-14 10:06:54 +0530

    update

--- rem_dup_lines.py
+++ rem_dup_lines.py
@@ -1,3 +1,4 @@
+import os
 openFile = open("pnl.txt", "r") 
 writeFile = open("pnl_1.txt", "w") 
 #Store traversed lines
@@ -10,3 +11,7 @@ for txtLine in openFile:
         tmp.add(txtLine)         
 openFile.close() 
 writeFile.close()
+os.remove("pnl.txt")
+old_name = r"pnl_1.txt"
+new_name = r"pnl.txt"
+os.rename(old_name, new_name)
