d8fc86d5b283037b1617afde92758e5b8e4c3ed5
tradesivas <tradesivas@gmail.com>
2023-02-20 15:32:11 +0530

    update

--- stradle_chart.py
+++ stradle_chart.py
@@ -4,9 +4,10 @@ from dotenv import load_dotenv
 import pandas as pd
 import time
 from datetime import datetime
-# import matplotlib.pyplot as plt
+import matplotlib.pyplot as plt
 # import matplotlib.animation as animation
 # from matplotlib import style
+from matplotlib.animation import FuncAnimation
 
 #####################################################################
 
@@ -63,17 +64,18 @@ df1 = pd.DataFrame([message1])
 df1.columns = ['null_0', 'opt_token', 'null_2', 'null_3', 'null_4', 'null_5', 'ltp', 'null_7', \
                 'null_8', 'null_9', 'null_10', 'null_11', 'null_12', 'null_13', 'null_14', 'null_15', \
                 'null_16', 'null_17', 'null_18', 'null_19', 'null_20', 'null_21', 'null_22']
-pldf1 = pd.DataFrame(data={'time':[''],'pnl':['']})
+pldf1 = pd.DataFrame(data={'Time':[''],'Price':['']})
 
 ##############################################################################
 t = 0
-
+x = list()
+y = list()
 ###############################################################################
 issell = 0
 try:
     def callback_method(message):
         #print(message)
-        global df, df1, ltp, celtp, peltp, issell, cesold, pesold, stradle_premium, sold_stradle_premium, time_str, time_obj, time1_str, time1_obj, pnl, fig, ax1,ani, pldf, pldf1, t
+        global df, df1, ltp, celtp, peltp, issell, cesold, pesold, stradle_premium, sold_stradle_premium, time_str, time_obj, time1_str, time1_obj, pnl, fig, ax1,ani, pldf, pldf1, t, x , y, ct
         #print("Your logic/computation will come here.")
         #print(type(message))
         df = pd.DataFrame([message])
@@ -94,59 +96,42 @@ try:
         #print(df1)
         celtp = float(df1.loc[(df1['opt_token'] == ceinstrumentToken), 'null_2'].iloc[0])
         peltp = float(df1.loc[(df1['opt_token'] == peinstrumentToken), 'null_2'].iloc[0])
-        stradle_premium = celtp + peltp
+        time1 = (df1.loc[(df1['opt_token'] == peinstrumentToken), 'null_19'].iloc[0])
+        ct = datetime.now().strftime("%H:%M:%S")
+        stradle_premium = round((celtp + peltp),0)
         print(strike,"CE LTP = ", celtp)
         print(strike,"PE LTP = ", peltp)
         print("Stradle Premium = ", stradle_premium)
         ###################################################################################
-        if issell == 0:
-            ###################################################
-            # order_response = client.place_order(order_type = "N", instrument_token = ceinstrumentToken_int, transaction_type = "SELL",\
-            #        quantity = lot, price = 0, disclosed_quantity = 0, trigger_price = 0,\
-            #        tag = "bnf_3pm_ex.py", validity = "GFD", variety = "REGULAR")
-            # print("Sell order placed for BANKNIFTY Option CE")
-            # print(order_response)
-            # o_id = order_response['Success']['NSE']['orderId']
-            # print("OrderId= ",o_id)
-            # netq = 99999
-            # while netq != 0:
-            #     o_r=client.order_report(order_id = o_id)
-            #     print(o_r)
-            #     l1 = len(o_r['success'])
-            #     print("Lenth = ",l1)
-            #     netq = (o_r['success'][(l1-1)]['orderQuantity']) - (o_r['success'][(l1-1)]['filledQuantity'])
-            #     print(netq)
-            #     time.sleep(1)
-            print("Sell order Filled for CE at Rs.", celtp, " * ", lot, " = Rs.", (celtp*lot))
-            print("Sell order filled for PE at Rs.", peltp, " * ", lot, " = Rs.", (peltp*lot))
-            issell = 1
-            cesold = celtp
-            pesold = peltp
-            sold_stradle_premium = cesold + pesold
-            print("Premium Collected = ", "%.2f" % (sold_stradle_premium * lot))     
-                            ###########
-            # sl_order_response = client.place_order(order_type = "N", instrument_token = ceinstrumentToken_int, transaction_type = "Buy",\
-            #        quantity = lot, price = 0, disclosed_quantity = 0, trigger_price = (2.5*cesold),\
-            #        tag = "bnf_3pm_ex.py", validity = "GFD", variety = "REGULAR")
+        # time1_str = df.iloc[-1]['null_19']
+        # time1_obj = datetime.strptime(time1_str, '%d/%m/%Y %H:%M:%S')
+        # pldf = pd.DataFrame(data={'Time':[time1_str],'Price':[stradle_premium]})
+        # pldf1 = pd.concat([pldf,pldf1])
+        # pldf1.to_csv(r"stradle.csv", sep=',', mode='a', header = True, index = False)
 
-            ###################################################
-
-        elif issell == 1:
-            print("Already Sold stradle at Rs.", sold_stradle_premium, " * ", lot, " = Rs.", "%.2f" % (sold_stradle_premium*lot))
-            pnl = sold_stradle_premium - stradle_premium
-            print("P&L= Rs.", "%.2f" % (pnl*lot) )
-            time1_str = df.iloc[-1]['null_19']
-            time1_obj = datetime.strptime(time1_str, '%d/%m/%Y %H:%M:%S')
-            t+= 1
-            pldf = pd.DataFrame(data={'time':[t],'pnl':[pnl]})
-            pldf1 = pd.concat([pldf,pldf1])
-            pldf1.to_csv(r"pnl.txt", sep=',', mode='a', header = False, index = False)
-
-            
-        else:
-            print("No TRADE")
         ########################################################
+        x.append(ct)
+        y.append(stradle_premium)
+        t+=1
     client.subscribe(input_tokens=ws_token, callback=callback_method, broadcast_host="https://wstreamer.kotaksecurities.com/feed")
+
+
+    plt.style.use('fivethirtyeight')
+
+    def animate(i):
+        plt.cla()
+        plt.plot(x, y)
+
+
+    ani = FuncAnimation(plt.gcf(), animate, 1000)
+
+
+    plt.tight_layout()
+    plt.show()
+
+
+
+
 
     # style.use('fivethirtyeight')
 
